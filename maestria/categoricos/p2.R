b9ee70f2028885457a8203ad2ff718a7963d5503
bmfazio <boris.fazio@outlook.com>
2017-10-16 10:51:49 -0500

    Update p2.R

--- maestria/categoricos/p2.R
+++ maestria/categoricos/p2.R
@@ -1,3 +1,32 @@
+# P2 
+
+**a) Bayes**
+
+Dada una variable $Y \sim \text{Bernoulli}(\pi)$, se tiene un modelo de regresión logística si podemos expresar $g(\mathbb E [Y \mid X = x]) = \text{logit}(\text P (Y = 1 \mid X = x))$ en funcion de un predictor lineal $\eta_x$.
+
+Comenzamos expandiendo los terminos al interior de la funcion de enlace:
+
+$$\begin{align}
+\text{logit}(\text P (Y = 1 \mid X = x)) &= \log\left(\frac{\text P (Y = 1 \mid X = x)}{\text P (Y = 0 \mid X = x)}\right)\\
+&= \log\left(\frac{\text P (X = x \mid Y = 1) \text P (Y = 1)}{\text P (X = x \mid Y = 0) \text P (Y = 0)}\right)\\
+&= \log\left(\frac{\text P (Y = 1)}{\text P (Y = 0)}\right) + \log\left(\frac{\text P (X = x \mid Y = 1)}{\text P (X = x \mid Y = 0)}\right)
+\end{align}$$
+
+El enunciado nos da las distribuciones de $X \mid Y$ y el parametro $\pi$ para $Y$, reemplazamos:
+
+$$\begin{align}
+\text{logit}(\text P (Y = 1 \mid X = x)) &= \log\left(\frac{\pi}{1-\pi}\right) + \log\left(\exp\left[\frac{x^2-(x-\mu)^2}{2}\right]\right)\\
+&= \log\left(\frac{\pi}{1-\pi}\right) - \frac{\mu^2}{2} + \mu x
+\end{align}$$
+
+Y reconocemos que la expresion toma la forma de un predictor lineal $\eta_x = \beta_0 + \beta_1x$ con
+
+$$\begin{align}
+\beta_0 &= \log\left(\frac{\pi}{1-\pi}\right) - \frac{\mu^2}{2}\\
+\beta_1 &= \mu
+\end{align}$$
+
+
 # Problema 3 - Simulaciones
 
   # a) Generar bases de datos
